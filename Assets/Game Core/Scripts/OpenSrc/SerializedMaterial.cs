16e9b2f74c9521e94f0e5f01b994c9ae88f10475
ReDark Technology <53262904+ReDarkTechnology@users.noreply.github.com>
2021-12-03 16:23:31 +0700

    Fixing Serializing error

---
+++ Assets/Game Core/Scripts/OpenSrc/SerializedMaterial.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class SerializedMaterial
+{
+    public Material material;
+    public Shader shader;
+    public Color mainColor;
+    public void ChangeValues()
+    {
+    }
+}
---
+++ Assets/Game Core/Scripts/OpenSrc/World/SerializedWorld.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class SerializedWorld
+{
+    public string worldName;
+    public string[] worldAuthors;
+    public SerializedGameObject[] worldObjects;
+}
